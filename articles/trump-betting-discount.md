1430f7f3986f59127b373c5bf288a86cf43e9d1d
coltonmilbrandt <coltonmilbrandt@gmail.com>
2024-07-02 23:23:25 -0600

    article add

---
+++ articles/trump-betting-discount.md
@@ -0,0 +1,33 @@
+---
+title: "Trump Betting Odds May be at a Massive Discount"
+date: "2024-07-02"
+author: "Colton Milbrandt"
+website: "www.coltonmilbrandt.com"
+bio: "Analyst and consultant who loves making the complex future a little less uncertain."
+---
+
+![Dice & Chips](/dice-and-chips.jpg)
+
+## Milbrandt Analytics Election Model is At Odds with Vegas
+
+Real value may be on the table with the Trump betting odds. Those of us paying attention might be shocked to find that Real Clear Politics has Trump with just a 56.2% chance of winning the election. Our model indicates Trump has an 81.7% chance of winning, indicating a massive discount, with favorable polling in key states. It’s hard to argue with Vegas, but with an upside potential of nearly 78%, those odds are looking rather attractive. Let’s dive into why.
+
+### The Competition is Decimated
+
+Biden, currently Trump’s next closest challenger, has taken an absolute nosedive to a mere 16.5% chance of winning the election after a historically bad debate performance. He is followed up by Kamala Harris (10.5%), Gavin Newsom (5.7%), Michelle Obama (3.6%), and a slew of other potential fly-by-night Democratic nominees. The Democrat party is in utter disarray, and with just four months to unify behind a candidate, they are losing ground every day. Meanwhile, Trump is gaining steam, and talk of an inevitable Trump presidency is setting in. The only potential challenger who has shown any promise in the polls, former First Lady Michelle Obama, has been rumored to be against the idea of running entirely. Unfortunately for Democrats, at this point, there is no plan for defense at all.
+
+### Biden Has Become Embarrassing
+
+Nobody with respect for the office of the presidency of the United States liked the look of the first presidential debate. Biden confirmed himself to many as an embarrassment, and talk has it that Democrats up and down the ticket are actively going on the defensive in their own party, making efforts to distance themselves from Biden. With that kind of fracturing surrounding the central figure of the party, Democrats could find themselves lacking the leadership to unify behind anyone whatsoever, and meanwhile, large portions of the voter base are settling in on Trump. Trump is now up 2.4% in the RCP Average, which is impressive in itself, but translates to an even bigger electoral win.
+
+### Our Election Simulation Predicts a Strong Trump Showing
+
+Trump was already gaining considerable ground prior to the debate with even Nate Silver giving him a 67% chance to win. Our model currently shows Trump at 81.7%. With a firm hold in Arizona, Georgia, and Nevada, Trump will be freed up to go on the offensive with his massive arsenal of money raised. With a strategic defense in those states, a strong campaign in Pennsylvania and the Rust Belt could all but assure his victory. As of now, a simple win in Michigan could be enough to carry him over the top, and the momentum is his for the taking.
+
+### The Payoff
+
+A bet on Trump at a 56.2% chance of winning would yield a 77.9% return, i.e., a $100 bet would yield $177.90. Given our model’s 81.7% chance for Trump to win, this $100 bet on Trump represents an expected value of $145.37. This could be a good value as a hedge against the uncertainty that a Trump Presidency would bring. For example, worries about a slowdown in economic growth due to slowed immigration or even deportation could be somewhat offset. Gambling is not recommended, but to the savvy speculator, a negative correlation with other bets could make this attractive.
+
+**Disclaimer:**
+
+_The information provided in this article is for informational and entertainment purposes only and does not constitute financial, investment, or betting advice. The views expressed are based on personal opinions and interpretations of current events and data. Betting on political outcomes carries inherent risks, and the odds and predictions presented are subject to change. Always conduct your own research and consider your own financial situation before placing any bets. The author and publisher of this article are not responsible for any financial losses or damages incurred as a result of following the information or suggestions provided. Gambling can be addictive; if you or someone you know has a gambling problem, please seek help from a professional._
--- src/app/home-page.js
+++ src/app/home-page.js
@@ -7,10 +7,14 @@ import logo from "../../public/transparent-logo.png"
 import MarketCharts from "../components/MarketCharts.js"
 import LeftCover from "../components/LeftCover.js"
 import NewsBox from "../components/NewsBox"
+import ContentBox from "../components/ContentBox"
 
 // ########### News Article Photos ########### //
 import trumpVsBiden from "../../public/trumpVsBiden.jpg"
 import chips from "../../public/chips.jpg"
+import electionSimJPG from "../../public/electionSim.jpg"
+import usaMapJPG from "../../public/usa-map.jpg"
+import diceAndChips from "../../public/dice-and-chips.jpg"
 
 const Home = () => {
 	return (
@@ -90,10 +94,10 @@ const Home = () => {
 										category="Politics"
 									/>
 									<NewsBox
-										href="/articles/trump-key-victory"
-										imageSrc={trumpVsBiden} // Ensure you have a relevant image imported
+										href="/articles/trump-betting-discount"
+										imageSrc={diceAndChips} // Ensure you have a relevant image imported
 										imageAlt="Campaign rally"
-										title="Trump's Key to Victory: Winning This State Could Seal the Deal"
+										title="Trump Betting Odds May be at a Massive Discount"
 										description="An in-depth look at the pivotal state that could determine Trump's path to victory in the upcoming election."
 										category="Politics"
 									/>
@@ -120,208 +124,58 @@ const Home = () => {
 								<div class="w-full py-3">
 									<h2 class="text-gray-800 text-2xl font-bold">
 										<span class="inline-block h-5 border-l-3 border-red-600 mr-2"></span>
-										Europe
+										Election Models
 									</h2>
 								</div>
 								<div class="flex flex-row flex-wrap -mx-3">
-									<div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-										<div class="flex flex-row sm:block hover-img">
-											<a href="">
-												<Image
-													class="max-w-full w-full mx-auto"
-													src={heroImage}
-													alt="alt title"
-												/>
-											</a>
-											<div class="py-0 sm:py-3 pl-3 sm:pl-0">
-												<h3 class="text-lg font-bold leading-tight mb-2">
-													<a href="#">
-														5 Tips to Save Money
-														Booking Your Next Hotel
-														Room
-													</a>
-												</h3>
-												<p class="hidden md:block text-gray-600 leading-tight mb-1">
-													This is a wider card with
-													supporting text below as a
-													natural lead-in to
-													additional content.
-												</p>
-												<a
-													class="text-gray-500"
-													href="#"
-												>
-													<span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>
-													Europe
-												</a>
-											</div>
-										</div>
-									</div>
-									<div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-										<div class="flex flex-row sm:block hover-img">
-											<a href="">
-												<Image
-													class="max-w-full w-full mx-auto"
-													src={heroImage}
-													alt="alt title"
-												/>
-											</a>
-											<div class="py-0 sm:py-3 pl-3 sm:pl-0">
-												<h3 class="text-lg font-bold leading-tight mb-2">
-													<a href="#">
-														5 Tips to Save Money
-														Booking Your Next Hotel
-														Room
-													</a>
-												</h3>
-												<p class="hidden md:block text-gray-600 leading-tight mb-1">
-													This is a wider card with
-													supporting text below as a
-													natural lead-in to
-													additional content.
-												</p>
-												<a
-													class="text-gray-500"
-													href="#"
-												>
-													<span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>
-													Europe
-												</a>
-											</div>
-										</div>
-									</div>
-									<div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-										<div class="flex flex-row sm:block hover-img">
-											<a href="">
-												<Image
-													class="max-w-full w-full mx-auto"
-													src={heroImage}
-													alt="alt title"
-												/>
-											</a>
-											<div class="py-0 sm:py-3 pl-3 sm:pl-0">
-												<h3 class="text-lg font-bold leading-tight mb-2">
-													<a href="#">
-														5 Tips to Save Money
-														Booking Your Next Hotel
-														Room
-													</a>
-												</h3>
-												<p class="hidden md:block text-gray-600 leading-tight mb-1">
-													This is a wider card with
-													supporting text below as a
-													natural lead-in to
-													additional content.
-												</p>
-												<a
-													class="text-gray-500"
-													href="#"
-												>
-													<span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>
-													Europe
-												</a>
-											</div>
-										</div>
-									</div>
-									<div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-										<div class="flex flex-row sm:block hover-img">
-											<a href="">
-												<Image
-													class="max-w-full w-full mx-auto"
-													src={heroImage}
-													alt="alt title"
-												/>
-											</a>
-											<div class="py-0 sm:py-3 pl-3 sm:pl-0">
-												<h3 class="text-lg font-bold leading-tight mb-2">
-													<a href="#">
-														5 Tips to Save Money
-														Booking Your Next Hotel
-														Room
-													</a>
-												</h3>
-												<p class="hidden md:block text-gray-600 leading-tight mb-1">
-													This is a wider card with
-													supporting text below as a
-													natural lead-in to
-													additional content.
-												</p>
-												<a
-													class="text-gray-500"
-													href="#"
-												>
-													<span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>
-													Europe
-												</a>
-											</div>
-										</div>
-									</div>
-									<div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-										<div class="flex flex-row sm:block hover-img">
-											<a href="">
-												<Image
-													class="max-w-full w-full mx-auto"
-													src={heroImage}
-													alt="alt title"
-												/>
-											</a>
-											<div class="py-0 sm:py-3 pl-3 sm:pl-0">
-												<h3 class="text-lg font-bold leading-tight mb-2">
-													<a href="#">
-														5 Tips to Save Money
-														Booking Your Next Hotel
-														Room
-													</a>
-												</h3>
-												<p class="hidden md:block text-gray-600 leading-tight mb-1">
-													This is a wider card with
-													supporting text below as a
-													natural lead-in to
-													additional content.
-												</p>
-												<a
-													class="text-gray-500"
-													href="#"
-												>
-													<span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>
-													Europe
-												</a>
-											</div>
-										</div>
-									</div>
-									<div class="flex-shrink max-w-full w-full sm:w-1/3 px-3 pb-3 pt-3 sm:pt-0 border-b-2 sm:border-b-0 border-dotted border-gray-100">
-										<div class="flex flex-row sm:block hover-img">
-											<a href="">
-												<Image
-													class="max-w-full w-full mx-auto"
-													src={heroImage}
-													alt="alt title"
-												/>
-											</a>
-											<div class="py-0 sm:py-3 pl-3 sm:pl-0">
-												<h3 class="text-lg font-bold leading-tight mb-2">
-													<a href="#">
-														5 Tips to Save Money
-														Booking Your Next Hotel
-														Room
-													</a>
-												</h3>
-												<p class="hidden md:block text-gray-600 leading-tight mb-1">
-													This is a wider card with
-													supporting text below as a
-													natural lead-in to
-													additional content.
-												</p>
-												<a
-													class="text-gray-500"
-													href="#"
-												>
-													<span class="inline-block h-3 border-l-2 border-red-600 mr-2"></span>
-													Europe
-												</a>
-											</div>
-										</div>
-									</div>
+									<ContentBox
+										href="/articles/trump-achilles-heel"
+										imageSrc={electionSimJPG} // Ensure you have a relevant image imported
+										imageAlt="Election Histogram Chart"
+										title="Election Simulation Results: Trump Comes Out Ahead"
+										description="Trump averages 294 electoral votes, but borders on a landslide win with a potential Rust-Belt sweep."
+										category="Politics"
+									/>
+									<ContentBox
+										href="/articles/trump-achilles-heel"
+										imageSrc={usaMapJPG} // Ensure you have a relevant image imported
+										imageAlt="Political debate"
+										title="Trump's Must-Win States"
+										description="Where should Trump campaign hardest? An analysis of the states that contribute the most statistically to a Trump win."
+										category="Politics"
+									/>
+									<ContentBox
+										href="/articles/trump-achilles-heel"
+										imageSrc={trumpVsBiden} // Ensure you have a relevant image imported
+										imageAlt="Political debate"
+										title="Biden's Must-Win States"
+										description="An analysis of the key vulnerabilities and challenges facing Trump's campaign as he vies for the presidency."
+										category="Politics"
+									/>
+									<ContentBox
+										href="/articles/trump-achilles-heel"
+										imageSrc={trumpVsBiden} // Ensure you have a relevant image imported
+										imageAlt="Political debate"
+										title="Trump Undervalued? 20% Upside on Trump Betting Odds"
+										description="An analysis of the key vulnerabilities and challenges facing Trump's campaign as he vies for the presidency."
+										category="Politics"
+									/>
+									<ContentBox
+										href="/articles/trump-achilles-heel"
+										imageSrc={trumpVsBiden} // Ensure you have a relevant image imported
+										imageAlt="Political debate"
+										title="Trump's Achilles Heel: The Challenges That Could Derail His Campaign"
+										description="An analysis of the key vulnerabilities and challenges facing Trump's campaign as he vies for the presidency."
+										category="Politics"
+									/>
+									<ContentBox
+										href="/articles/trump-achilles-heel"
+										imageSrc={trumpVsBiden} // Ensure you have a relevant image imported
+										imageAlt="Political debate"
+										title="Trump's Achilles Heel: The Challenges That Could Derail His Campaign"
+										description="An analysis of the key vulnerabilities and challenges facing Trump's campaign as he vies for the presidency."
+										category="Politics"
+									/>
 								</div>
 							</div>
 							{/* <!-- right --> */}
